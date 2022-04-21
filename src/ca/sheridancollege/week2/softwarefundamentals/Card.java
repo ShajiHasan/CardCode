7a45fbd846cda034754d835cef78deea70b3c8e4
shaji <hasashaj@sheridancollege.ca>
2022-04-21 19:30:46 -0400

    added a comment

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -10,6 +10,12 @@ package ca.sheridancollege.week2.softwarefundamentals;
  * Created in Week 2 for the cloning repository demo
  * @author dancye, 2019
  */
+
+/*
+ * Shaji Hasan
+ * Adding comment 
+ */
+
 public class Card 
 {
     private String suit;// hearts, diamonds, spades and clubs
