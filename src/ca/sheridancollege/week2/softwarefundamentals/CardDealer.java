45185974a51aa8178274e03f982630bbffe30fdf
shaji <hasashaj@sheridancollege.ca>
2022-04-22 02:31:47 -0400

    Added a card object

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Shaji Hasan
+ * 991441275
  */
 package ca.sheridancollege.week2.softwarefundamentals;
 
@@ -15,5 +14,7 @@ public class CardDealer
     {
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
+        
+        Card sevenHearts = new Card("Hearts", 7);
     }
 }
